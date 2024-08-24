f6ccfbff274f4472e798e3fced942db6dbdf5ad6
Billy-Bek <bekk1bek198@gmail.com>
2024-08-24 00:00:27 -0700

    feat: mongodb ulanish

--- server.js
+++ server.js
@@ -1,53 +1,19 @@
-console.log("Wep serverni  boshlash");
-const { log } = require("console");
-const express = require("express"); // express bu function & veriable
-const app = express(); // expressning app objectini yuboradi.
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb")
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
+let connectionString = "mongodb+srv://Billy:fbjnqQmbJMG6mGQL@cluster0.rhbl7.mongodb.net/reja"
+mongodb.connect(connectionString, {useNewUrlParser: true,
+     useUnifiedTopology: true}, (err, client) =>{
+if(err) console.log("ERROR on conection MongoDB")
+    else {
+    console.log("MongoDB connection succed")
+    module.exports = client;
+    const app = require("./app")
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+          console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        });
+    }
+     })
 
-// 1: Kirish
-app.use(express.static("public"));
-app.use(express.static('public'));
-
-app.use(express.json()); //json farmatdagi datani objectga ogirib beradi
-app.use(express.urlencoded({ extended: true })); // hrml, formdan post qilgan narsani qabul qd //  true nest objectni ochib beradi.
-
-// 2: Session code
-
-// 3: Views code
-// backendda html yasab clientga jo'natamiz
-app.set("views", "views"); //frontetimizni views folderdan topish
-app.set("view engine", "ejs"); // ejs orqali frontetni yasaymiz
-
-// ********************************* //
-
-// 4: Routing code
-// Jonatadi
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-// qabul qiladi
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
